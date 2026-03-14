d3ffa7538aa5e71258e97cb48119f7672006ba58
sanjanayadav07 <sanjanayadav3952@gmail.com>
2026-03-14 11:50:44 +0530

    Updated portfolio changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -67,12 +67,12 @@ export default function Contact() {
   };
 
   const quickLinks = [
-    { img: githubLogo, title: "GitHub", link: "https://github.com/kunj2803" },
-    { img: linkedinLogo, title: "LinkedIn", link: "https://www.linkedin.com/in/kunj-desai-07717b293/" },
-    { img: gmailLogo, title: "Email", link: "mailto:kunjd2803@gmail.com" },
-    { img: whatsappLogo, title: "WhatsApp", link: "https://wa.me/+918758209508" },
-    { img: instagramLogo, title: "Instagram", link: "https://www.instagram.com/kunj_2834/" },
-    { img: facebookLogo, title: "Facebook", link: "https://www.facebook.com/kunj.desai.222608" },
+    { img: githubLogo, title: "GitHub", link: "https://github.com/Sanjanayadav07" },
+    { img: linkedinLogo, title: "LinkedIn", link: "https://www.linkedin.com/in/sanjana-yadav007/" },
+    { img: gmailLogo, title: "Email", link: "mailto:sanjanayadav3952@gmail.com" },
+    { img: whatsappLogo, title: "WhatsApp", link: "https://wa.me/+918090657623" },
+    { img: instagramLogo, title: "Instagram", link: "https://www.instagram.com/itx_sanjana_07/" },
+    { img: facebookLogo, title: "Facebook", link: "#" },
   ];
 
   return (
